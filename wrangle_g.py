4dab18c21bd9b91a5473bb3c614f35641ea42ec5
“Garrett <garrett.paul.arnett@gmail.com>
2023-05-15 11:38:31 -0500

    .

--- wrangle_g.py
+++ wrangle_g.py
@@ -7,6 +7,12 @@ from scipy import stats
 from sklearn.model_selection import train_test_split
 import os
 from langdetect import detect
+import unicodedata
+import re
+import nltk
+from bs4 import BeautifulSoup
+import requests
+from nltk.corpus import stopwords
 seed = 1349
 
 
@@ -46,3 +52,66 @@ def prepare_mvp():
     df = df[df['readme_language'] == 'en']
     df = df.drop('readme_language', axis=1)
     return df
+
+def basic_clean(string):
+    '''
+    takes in a string and outputs a basic-cleaned version:
+                    -lowercase
+                    -normalized to unicode set
+                    -replaced non-word and non-singlespace,non-singlequote chars with ''
+    '''
+    lowered = string.lower()
+    normalized = unicodedata.normalize('NFKD', lowered).encode('ascii','ignore').decode('utf-8')
+    basic_clean = re.sub(r'[^a-zA-Z0-9\s]', '', normalized)
+    return basic_clean
+
+def tokenize(string):
+    '''
+    takes in a string and outputs a tokenized version:
+    
+    '''
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    tokenized = tokenizer.tokenize(string, return_str=True)
+    return tokenized
+
+def remove_stopwords(some_string, extra_words=['r','be', 'python'], keep_words=[]):
+    '''
+    remove stopwords will take in a single document as a string
+    and return a new string that has stopwords removed
+    '''
+    stopwords_custom = set(stopwords.words('english')) - \
+    set(keep_words)
+    stopwords_custom = list(stopwords_custom.union(extra_words))
+    return ' '.join([word for word in some_string.split()
+                     if word not in stopwords_custom])
+
+def lemmatize(some_string):
+    '''
+    lemmatize will take in the contents of a single string,
+    split up the contents with split()
+    use the split contents as a list to apply a lemmatizer to
+    each word,
+    and return a single string of the lemmatized words joined
+    with a single instance of whitespace (' '.join())
+    '''
+    lemmatizer = nltk.WordNetLemmatizer()
+    return ' '.join(
+        [lemmatizer.lemmatize(word,'v'
+                             ) for word in some_string.split()])
+
+def final_wrangle(df):
+    df['cleaned'] = df.readme_contents.apply(basic_clean)
+    df['tokenized'] = df.cleaned.apply(tokenize)
+    df['lemmatized'] = df.tokenized.apply(lemmatize)
+    df['stopped'] = df.lemmatized.apply(remove_stopwords)
+    df =df.drop(columns=['cleaned','tokenized','lemmatized'])
+    X = df.stopped
+    y = df.language
+    X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, 
+                 test_size=0.2, 
+                 random_state=1349)
+    
+    return X_train, X_test, y_train, y_test
+
+
